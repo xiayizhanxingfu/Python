f26095ab85771ceed7115dc6d14b839dfe044794
2963059696@qq.com <2963059696@qq.com>
2019-09-07 22:30:27 +0800

    request
    2019/9/7

---
+++ demo.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+date={
+	'username':'2963059696',
+	'password':'15136651167zkq'
+}
+repsone = requests.get('C:\\Users\\zhangkaiqi\\Desktop\\date.html')
+# soup = BeautifulSoup(html_doc,'lxml')
+#print(soup.prettify())
---
+++ 快速上手 — Requests 2.18.1 文档/快速上手 — Requests 2.18.1 文档_files/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "requests-docs-cn",
+    version: "latest",
+    language: "zh_CN",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "http://docs.python-requests.org/zh_CN/latest/",
+    theme: "alabaster",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "6175fad9",
+    ad_free: false,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: null
+};
+
